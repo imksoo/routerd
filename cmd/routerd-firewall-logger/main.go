a458270485a242a555315263b5124c0551987d5d
imksoo <kirino.minato@gmail.com>
2026-05-05 12:09:00 +0000

    Phase 3.1c: keep tcpdump under systemd privileges

--- cmd/routerd-firewall-logger/main.go
+++ cmd/routerd-firewall-logger/main.go
@@ -75,7 +75,7 @@ func daemon(args []string, stdin io.Reader) error {
 		defer file.Close()
 		reader = file
 	} else if opts.pflogInterface != "" {
-		cmd = exec.Command(opts.tcpdumpPath, "-n", "-e", "-tttt", "-l", "-i", opts.pflogInterface)
+		cmd = exec.Command(opts.tcpdumpPath, "-n", "-e", "-tttt", "-l", "-Z", "root", "-i", opts.pflogInterface)
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
 			return err
@@ -90,7 +90,7 @@ func daemon(args []string, stdin io.Reader) error {
 		}()
 		reader = stdout
 	} else if opts.group > 0 {
-		cmd = exec.Command(opts.tcpdumpPath, "-n", "-tttt", "-l", "-i", "nflog:"+strconv.Itoa(opts.group))
+		cmd = exec.Command(opts.tcpdumpPath, "-n", "-tttt", "-l", "-Z", "root", "-i", "nflog:"+strconv.Itoa(opts.group))
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
 			return err
